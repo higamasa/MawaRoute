b05c4feca06a1a55af22da016b6a65f8e0cecf0d
murata astusuke <kuroppems@gmail.com>
2016-01-08 17:11:40 +0900

    無駄な行を削除しました。 MainFragmentPagerAdapterクラスのgetItemメソッドにあるTestPage1 page1 = new TestPage1();return page1;はインライン化しました。

--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
@@ -11,20 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //関数名決めてないからonCreateに直接書いた by村田 2015 12/31
+        //TabとSwipeの読み込み
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager(),
                 MainActivity.this));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
     }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-
-    }
-
 }
--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainFragmentPagerAdapter.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainFragmentPagerAdapter.java
@@ -5,23 +5,18 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-
 /**
  * Created by atsusuke on 2015/12/31.
  */
-
 public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
-
     final int PAGE_COUNT = 4;
     private String tabTitles[] = new String[] { "L", "G", "R", "S" };
     private Context context;
-
 
     public MainFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
     }
-
 
     @Override
     public int getCount() {
@@ -32,20 +27,13 @@ public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
         Bundle data = new Bundle();
         switch(arg0){
             case 0:
-                TestPage1 page1 = new TestPage1();
-                return page1;
-
+                return new TestPage1();
             case 1:
-                TestPage2 page2 = new TestPage2();
-                return page2;
-
+                return new TestPage2();
             case 2:
-                TestPage3 page3 = new TestPage3();
-                return page3;
+                return new TestPage3();
             case 3:
-                TestPage4 page4 = new TestPage4();
-                return page4;
-
+                return new TestPage4();
         }
         return null;
     }
