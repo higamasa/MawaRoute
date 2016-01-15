50597de107683f79e0f0ae4b4678b0ef27411602
KoizumiYunosuke <b1501498@planet.kanazawa-it.ac.jp>
2016-01-15 16:00:19 +0900

    ぐるナビデータの取得と、店検索

---
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/gnavi/Position.java
@@ -0,0 +1,14 @@
+package com.example.owner_pc.androidkanazawa2015.gnavi;
+
+/**
+ * Created by owner-PC on 2016/01/15.
+ */
+public class Position {
+    public double latitude;
+    public double longitude;
+    public Position(){}
+    public Position(double latitude, double longitude){
+        this.latitude  = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/gnavi/ShopParameter.java
@@ -0,0 +1,58 @@
+package com.example.owner_pc.androidkanazawa2015.gnavi;
+
+/**
+ * Created by owner-PC on 2016/01/15.
+ */
+public class ShopParameter {
+
+    private String shopName;
+    private String shopCategory;
+    private String openTime;
+    private String shopAddress;
+    private String shopImage;
+    private Position position = new Position();
+
+    public void setShopName(String name){
+        shopName = name;
+    }
+    public void setShopCategory(String category){
+        shopCategory = category;
+    }
+    public void setOpenTime(String time){
+        openTime = time;
+    }
+    public void setShopAddress(String address){
+        shopAddress = address;
+    }
+    public void setShopImage(String image){
+        shopImage = image;
+    }
+    public void setLatitude(String latitude){
+        position.latitude = Double.parseDouble(latitude);
+    }
+    public void setLongitude(String longitude){
+        position.longitude = Double.parseDouble(longitude);
+    }
+
+    public String getShopName(){
+        return shopName;
+    }
+    public String getShopCategory(){
+        return shopCategory;
+    }
+    public String getOpenTime(){
+        return openTime;
+    }
+    public String getShopAddress(){
+        return shopAddress;
+    }
+    public String getShopImage(){
+        return shopImage;
+    }
+    public double getLatitude(){
+        return position.latitude;
+    }
+    public double getLongitude(){
+        return position.longitude;
+    }
+}
