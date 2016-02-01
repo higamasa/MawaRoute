0b24c180687f354b1386a3d2a8184a738e5eb997
murata astusuke <kuroppems@gmail.com>
2016-02-01 15:44:40 +0900

    位置情報取得の改善
    Map.javaで位置情報を取得していましたが、Activity→Fragmentにパラメータを渡す方が簡単だったので、MainActivityで位置情報を取得し、Map.javaにパラメータを渡すように変更しました。

--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.owner_pc.androidkanazawa2015;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
-import android.location.LocationListener;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
@@ -11,12 +11,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
 import com.example.owner_pc.androidkanazawa2015.gnavi.GnaviCtrl;
 import com.example.owner_pc.androidkanazawa2015.gnavi.Position;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +24,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //位置情報の取得
+        // LocationManagerを取得
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Criteriaオブジェクトを生成
+        Criteria criteria = new Criteria();
+        //位置情報の精度
+        criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+        //消費電力
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        //ロケーションプロバイダの取得
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //現在地取得
+        Location location = locationManager.getLastKnownLocation(provider);
+        Log.d("check", "緯度:" + location.getLatitude() + "軽度:" + location.getLongitude());
+        LatLng lat = new LatLng(location.getLatitude() , location.getLongitude());
+
         //TabとSwipeの読み込み
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
+        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()
+                , MainActivity.this , location.getLatitude() , location.getLongitude()));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -61,3 +86,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+
--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainFragmentPagerAdapter.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainFragmentPagerAdapter.java
@@ -6,16 +6,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import com.example.owner_pc.androidkanazawa2015.google_map.Map;
+import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by atsusuke on 2015/12/31.
  */
 public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
-    private String tabTitles[] = new String[] { "L", "G", "R"/*, "S"*/ };
+    private String tabTitles[] = new String[] { "L", "G", "R" };
     private Context context;
-    public MainFragmentPagerAdapter(FragmentManager fm, Context context) {
+    private double latitude;
+    private double longitude;
+
+    public MainFragmentPagerAdapter(FragmentManager fm, Context context , double latitude , double longitude) {
         super(fm);
         this.context = context;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
     @Override
     public int getCount() {
@@ -23,16 +29,18 @@ public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
     }
     @Override
     public Fragment getItem(int arg0) {
-        Bundle data = new Bundle();
+        Bundle bundle = new Bundle();
         switch(arg0){
             case 0:
                 return new TestPage1();
             case 1:
-                return new Map();
+                bundle.putDouble("latitude" , latitude);
+                bundle.putDouble("longitude" , longitude);
+                Map map = new Map();
+                map.setArguments(bundle);
+                return map;
             case 2:
                 return new TestPage3();
-           /* case 3:
-                return new TestPage4();*/
         }
         return null;
     }
