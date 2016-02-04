5a68c452c9bd9008cbd1e5bebdc5a2092dcf850b
KoizumiYunosuke <b1501498@planet.kanazawa-it.ac.jp>
2016-02-04 18:51:03 +0900

    AsyncTaskCallbacks add

---
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/gnavi/AsyncTaskCallbacks.java
@@ -0,0 +1,11 @@
+package com.example.owner_pc.androidkanazawa2015.gnavi;
+
+/**
+ * Created by owner-PC on 2016/02/03.
+ */
+public interface AsyncTaskCallbacks{
+    //終了
+    public void onTaskFinished();
+    //キャンセル
+    public void onTaskCancelled();
+}
