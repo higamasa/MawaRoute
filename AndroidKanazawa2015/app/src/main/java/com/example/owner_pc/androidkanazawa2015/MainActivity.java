0cb7f621ee2ce6e2a07d541c5b154c38f62d0932
KoizumiYunosuke <b1501498@planet.kanazawa-it.ac.jp>
2016-02-06 20:48:41 +0900

    画面下部のピンク色ボタン追加しました

--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.owner_pc.androidkanazawa2015;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -11,16 +13,24 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.owner_pc.androidkanazawa2015.gnavi.AsyncTaskCallbacks;
 import com.example.owner_pc.androidkanazawa2015.gnavi.GnaviCtrl;
 import com.example.owner_pc.androidkanazawa2015.gnavi.Position;
 import com.example.owner_pc.androidkanazawa2015.gnavi.ShopCtrl;
 
-public class MainActivity extends AppCompatActivity implements AsyncTaskCallbacks{
-    GnaviCtrl gnaviCtrl = new GnaviCtrl(this, this);
-    private double latitude = 36.594682;
+public class MainActivity extends AppCompatActivity implements AsyncTaskCallbacks, View.OnClickListener, ViewPager.OnPageChangeListener{
+
+    private GnaviCtrl gnaviCtrl = new GnaviCtrl(this, this);
+    private double latitude  = 36.594682;
     private double longitude = 136.625573;
+    private ImageButton rightButton = null;
+    private ImageButton leftButton  = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +51,13 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
         //TabとSwipeの読み込み
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()
-                , MainActivity.this, latitude, longitude , new ShopCtrl()));
+                , MainActivity.this, latitude, longitude, new ShopCtrl()));
+        viewPager.addOnPageChangeListener(this);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        //ボタンの表示
+        onDrawButton();
     }
 
     //ぐるナビ読み込み失敗
@@ -71,6 +85,92 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
                 this.gnaviCtrl.progressDialog.dismiss();
             }
         }
+    }
+
+    //画面下部のピンク色のボタン表示
+    private void onDrawButton(){
+
+        //スマホ画面サイズ取得
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        //ボタンに画像をセット
+        rightButton = (ImageButton)findViewById(R.id.right_button);
+        leftButton = (ImageButton)findViewById(R.id.left_button);
+        rightButton.setImageResource(R.drawable.under_button_p_r);
+        leftButton.setImageResource(R.drawable.under_button_p);
+
+        //ボタンの位置、グラビティ...etc取得
+        ViewGroup.LayoutParams[] params = new ViewGroup.LayoutParams[2];
+        params[0] = rightButton.getLayoutParams();
+        params[1] = leftButton.getLayoutParams();
+
+        //ボタンサイズ変更
+        for(int i = 0; i < 2; ++i){
+            params[i].width  = size.x / 3;
+            params[i].height = size.y / 3;
+        }
+
+        //サイズ変更を反映
+        rightButton.setLayoutParams(params[0]);
+        leftButton.setLayoutParams(params[1]);
+
+        //クリック可能
+        rightButton.setOnClickListener(this);
+        leftButton.setOnClickListener(this);
+
+    }
+
+    //ボタンが押されたらポップアップ表示
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.right_button:
+                Log.d("MainActivity", "pushed right");
+                //todo ポップアップ（距離、カテゴリ）
+                //todo listに条件絞り込みを渡す
+                break;
+            case R.id.left_button:
+                Log.d("MainActivity", "pushed left");
+                //todo ポップアップ（キーワード検索）
+                //todo listに条件絞り込みを渡す
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//        Log.d("MainActivity", "onPageScrolled() position="+position);
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("MainActivity", "onPageSelected() position="+position);
+        if(rightButton != null && leftButton != null) {
+            if (position == 1) {
+                //マップ画面時透過(灰色重ねる)
+                rightButton.setEnabled(false);
+                leftButton.setEnabled(false);
+                rightButton.setAlpha(0.5f);
+                leftButton.setAlpha(0.5f);
+                rightButton.setColorFilter(0xaa808080);
+                leftButton.setColorFilter(0xaa808080);
+            } else {
+                //リスト、ルーレット画面では通常表示
+                rightButton.setEnabled(true);
+                leftButton.setEnabled(true);
+                rightButton.setAlpha(1.0f);
+                leftButton.setAlpha(1.0f);
+                rightButton.setColorFilter(BIND_NOT_FOREGROUND);
+                leftButton.setColorFilter(BIND_NOT_FOREGROUND);
+            }
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+//        Log.d("MainActivity", "onPageScrollStateChanged() state="+state);
     }
 
     //位置情報の取得
