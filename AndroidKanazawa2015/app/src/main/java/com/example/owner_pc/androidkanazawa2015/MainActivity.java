904247390987157d26a90104d7fc0f2372725589
yuji_toyama <b1502433@planet.kanazawa-it.ac.jp>
2016-02-11 13:47:06 +0900

    設定用の新しいアクティビティとそのレイアウトとクラスを生成

--- AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
+++ AndroidKanazawa2015/app/src/main/java/com/example/owner_pc/androidkanazawa2015/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.owner_pc.androidkanazawa2015;
 
+import android.app.VoiceInteractor;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -10,7 +12,13 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import com.example.owner_pc.androidkanazawa2015.gnavi.AsyncTaskCallbacks;
@@ -18,29 +26,39 @@ import com.example.owner_pc.androidkanazawa2015.gnavi.GnaviCtrl;
 import com.example.owner_pc.androidkanazawa2015.gnavi.Position;
 import com.example.owner_pc.androidkanazawa2015.gnavi.ShopCtrl;
 
-public class MainActivity extends AppCompatActivity implements AsyncTaskCallbacks, ViewPager.OnPageChangeListener{
+public class MainActivity extends AppCompatActivity implements AsyncTaskCallbacks, ViewPager.OnPageChangeListener {
 
+    private Toolbar toolbar = null;
+    private SearchView mSearchView = null;
+    private Menu menu = null;
     private GnaviCtrl gnaviCtrl = new GnaviCtrl(this, this);
     private SettingButton settingButton = new SettingButton(this);
-    private double latitude  = 36.594682;
+    private double latitude = 36.594682;
     private double longitude = 136.625573;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //位置情報の読み込み
         //getLocation();
         Position position = new Position(latitude, longitude);
         //ぐるナビの読み込み
         gnaviCtrl.execute(position);
-
         //final ListView listView = (ListView)findViewById(R.id.list);
+
+        // ツールバー配置
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.search);
+        //menu = (Menu)toolbar.getMenu().findItem(R.id.action_settings).getActionView();
+        search();
     }
 
     //ぐるナビ読み込み完了
     @Override
-    public void onTaskFinished(){
+    public void onTaskFinished() {
         //TabとSwipeの読み込み
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()
@@ -50,12 +68,12 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
         tabLayout.setupWithViewPager(viewPager);
 
         //ボタンの表示
-        settingButton.onDrawButton();
+        //settingButton.onDrawButton();
     }
 
     //ぐるナビ読み込み失敗
     @Override
-    public void onTaskCancelled(){
+    public void onTaskCancelled() {
 
     }
 
@@ -66,8 +84,7 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
         Log.v("ActivityMain", "onPause()");
 
         // "プログレスダイアログ表示（シンプル）" のスレッド、プログレスダイアログが存在する場合
-        if (this.gnaviCtrl != null &&
-                this.gnaviCtrl.progressDialog != null) {
+        if (this.gnaviCtrl != null && this.gnaviCtrl.progressDialog != null) {
 
             Log.v("DialogShowing", String.valueOf(this.gnaviCtrl.progressDialog.isShowing()));
 
@@ -87,10 +104,10 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
 
     @Override
     public void onPageSelected(int position) {
-        Log.d("MainActivity", "onPageSelected() position="+position);
+        Log.d("MainActivity", "onPageSelected() position=" + position);
         ImageButton rightButton = settingButton.getRightButton();
-        ImageButton leftButton  = settingButton.getLeftButton();
-        if(rightButton != null && leftButton != null) {
+        ImageButton leftButton = settingButton.getLeftButton();
+        if (rightButton != null && leftButton != null) {
             if (position == 1) {
                 //マップ画面時透過(灰色重ねる)
                 rightButton.setEnabled(false);
@@ -130,19 +147,60 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskCallback
         String provider = locationManager.getBestProvider(criteria, true);
         //現在地取得
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         Location location = locationManager.getLastKnownLocation(provider);
 
         this.latitude = location.getLatitude();
         this.longitude = location.getLongitude();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent();
+            intent.setClassName("com.example.owner_pc.androidkanazawa2015", "com.example.owner_pc.androidkanazawa2015.range_category_settings");
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void search(){
+        mSearchView = (SearchView) toolbar.getMenu().findItem(R.id.menu_search).getActionView();
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 
 }
