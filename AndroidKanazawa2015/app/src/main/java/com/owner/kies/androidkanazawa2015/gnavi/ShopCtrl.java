fa16232307e213a82ae08d6e5c212e4f4d054a04
murata astusuke <kuroppems@gmail.com>
2016-03-30 01:05:27 +0900

    パッケージ名の変更

---
+++ AndroidKanazawa2015/app/src/main/java/com/owner/kies/androidkanazawa2015/gnavi/ShopCtrl.java
@@ -0,0 +1,59 @@
+package com.owner.kies.androidkanazawa2015.gnavi;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by owner-PC on 2016/01/15.
+ */
+public class ShopCtrl {
+    private static ArrayList<ShopParameter> shopList = new ArrayList<ShopParameter>();
+    private static ArrayList<RangeList> rangeList = new ArrayList<RangeList>();
+
+    //絞り込み条件を反映
+    public void categoryDividing() {
+        SettingParameter setting = new SettingParameter();
+        //店リスト初期化
+        shopList.clear();
+        //範囲距離、カテゴリ分け
+        int range = setting.getRangeType();
+        if (setting.isFastFood())  shopList.addAll(rangeList.get(range).fastFood);
+        if (setting.isCafe())      shopList.addAll(rangeList.get(range).cafe);
+        if (setting.isHighCal())   shopList.addAll(rangeList.get(range).highCal);
+        if (setting.isHighGrade()) shopList.addAll(rangeList.get(range).highGrade);
+        if (setting.isWine())      shopList.addAll(rangeList.get(range).wine);
+        if (setting.isOther())     shopList.addAll(rangeList.get(range).other);
+        //nullの店を削除
+        shopList.removeAll(Collections.singleton(null));
+        //キーワード検索
+        shopList = shopFiltering(setting.getKeyword());
+
+    }
+
+    public void setRangeList(ArrayList<RangeList> rangeList) {
+        this.rangeList = rangeList;
+    }
+
+    public ArrayList<ShopParameter> getShopList() {
+        return shopList;
+    }
+
+    //距離とカテゴリーキーワードを入力
+    private ArrayList<ShopParameter> shopFiltering(String keyword) {
+        if (keyword == null) {
+            return shopList;
+        }
+        ArrayList<ShopParameter> filterList = new ArrayList<ShopParameter>();
+        int size = shopList.size();
+        for (int i = 0; i < size; i++) {
+            if (shopList.get(i).getShopCategory().contains(keyword)
+                    || shopList.get(i).getShopName().contains(keyword)) {
+                filterList.add(shopList.get(i));
+            }
+        }
+        return filterList;
+    }
+}
---
+++ AndroidKanazawa2015/app/src/main/java/com/owner/kies/androidkanazawa2015/gnavi/ShopList.java
@@ -0,0 +1,10 @@
+package com.owner.kies.androidkanazawa2015.gnavi;
+
+import java.util.ArrayList;
+
+/**
+ * Created by owner-PC on 2016/01/15.
+ */
+public class ShopList {
+    public ArrayList<ShopParameter> shop = new ArrayList<ShopParameter>();
+}
