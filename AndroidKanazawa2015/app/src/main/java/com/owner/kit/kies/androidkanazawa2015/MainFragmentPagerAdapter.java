f13e254eadf96fb8fac9ea05a1c050ccd137700b
murata astusuke <kuroppems@gmail.com>
2016-04-12 18:45:03 +0900

    余分な行を削除

--- AndroidKanazawa2015/app/src/main/java/com/owner/kit/kies/androidkanazawa2015/MainFragmentPagerAdapter.java
+++ AndroidKanazawa2015/app/src/main/java/com/owner/kit/kies/androidkanazawa2015/MainFragmentPagerAdapter.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import com.owner.kit.kies.androidkanazawa2015.gnavi.ShopParameter;
