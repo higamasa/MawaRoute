0f93562f540bc217de348bb2330ae2bf05745a8f
ayatk <git@ayatk.com>
2017-09-29 21:20:11 +0900

    rangeパラメータを追加

--- AndroidKanazawa2015/app/src/main/java/kies/mawaroute/api/GnaviQuery.kt
+++ AndroidKanazawa2015/app/src/main/java/kies/mawaroute/api/GnaviQuery.kt
@@ -19,12 +19,25 @@ class GnaviQuery {
         return this
     }
 
+    fun range(range: Int): GnaviQuery {
+        // デフォルト 500m
+        var queryRange = 2
+        when (range) {
+            in 0 until 300 -> queryRange = 1
+            in 300 until 500 -> queryRange = 2
+            in 500 until 1000 -> queryRange = 3
+            in 1000 until 2000 -> queryRange = 4
+            in 2000 until 3000 -> queryRange = 5
+        }
+
+        query += Pair("range", queryRange.toString())
+        return this
+    }
+
     fun hitPerPage(size: Int): GnaviQuery {
         query += Pair("hit_per_page", size.toString())
         return this
     }
 
-    fun build(): Map<String, String> {
-        return query.toMap()
-    }
+    fun build(): Map<String, String> = query.toMap()
 }
